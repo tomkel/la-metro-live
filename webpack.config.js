7fd97ff6d62391d80733120c82c794ec076cbcd3
Tommy Kelly <tommy.kelly@comcast.com>
2017-09-15 16:12:42 -0700

    generate html with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,16 @@
 const { resolve } = require('path')
-const webpack = require('webpack')
 const cssnext = require('postcss-cssnext')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
-
-    'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
-    './index',
-    // the entry point of our app
-  ],
+  entry: './src/index',
   output: {
     filename: 'bundle.js',
-    // the output bundle
-
     path: resolve(__dirname, 'dist'),
-
-    publicPath: '/',
-    // necessary for HMR to know where to load the hot update chunks
   },
 
-  context: resolve(__dirname, 'src'),
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
-  },
-
-  devtool: 'inline-source-map',
-
-  devServer: {
-    hot: true,
-    // enable HMR on the server
-
-    contentBase: resolve(__dirname, 'dist'),
-    // match the output path
-
-    publicPath: '/',
-    // match the output `publicPath`
   },
 
   module: {
@@ -63,7 +30,7 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
-              plugins: [cssnext]
+              plugins: [cssnext],
             },
           },
         ],
@@ -76,10 +43,9 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
-
-    new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
+    new HtmlWebpackPlugin({
+      title: 'LA Metro Live Map',
+      template: 'src/index.ejs',
+    }),
   ],
 }
